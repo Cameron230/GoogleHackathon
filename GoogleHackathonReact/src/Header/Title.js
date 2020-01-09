cad02a7951c5695a87817e7deccc8173318c7ff9
Arslan Zahid <Arslan.Zahid@sainsburys.co.uk>
2020-01-09 11:25:15 +0000

    hells yeah

---
+++ GoogleHackathonReact/src/Header/Title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Title extends Component {
+    render() {
+        return (
+            <div> TITLE </div>
+        )
+    }
+}
+
+export default Title
